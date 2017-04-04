899697a5d7bc3a5f7fed90552b06c1641c57299c
gitedwinliam <edwinrosarioliam@gmail.com>
2017-04-04 00:56:10 -0700

    ppsuedo
    add psuedo code

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
 
+//create array and fill it up
 var cityList = ["NYC","SF","LA","ATX","SYD"];
 
+//populate options with values of the array
 for(i = 0; i < cityList.length; i++ ){
 	$("#city-type").append("<option>" + cityList[i] + "</option>");
 
 }
 
+//wait for a change in select
 $("#city-type").change(function(){
+	//create variable to get the changed value
 	var choice = $("#city-type").val();
-	console.log(choice);
 
+	//conditional loop to change value based on value of user
 	if(choice == "NYC"){
 		$('body').attr('class', 'nyc'); //sets class to nyc
 	}else if(choice == "SF"){
@@ -22,6 +26,7 @@ $("#city-type").change(function(){
 	}else if(choice == "SYD"){
 		$('body').attr('class', 'sydney');
 	}else{
+		//if user does not pick city, reset
 		$('body').attr('class','');
 	}
 })
